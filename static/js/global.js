06ad64d1498fda5e36110175d85f28ba5391cd74
NornsInteractive <NornsInteractive@ni.com>
2025-05-30 12:47:35 +0800

    基准2

--- static/js/global.js
+++ static/js/global.js
@@ -3,10 +3,10 @@ document.addEventListener('DOMContentLoaded', function() {
     // 动态加载 CSS
     const iziToastCss = document.createElement('link');
     iziToastCss.rel = 'stylesheet';
-    iziToastCss.href = '/static/libs/css/iziToast.min.css';
+    iziToastCss.href = 'static/libs/css/iziToast.min.css';
     document.head.appendChild(iziToastCss);
     // 动态加载 JS
     const iziToastScript = document.createElement('script');
-    iziToastScript.src = '/static/libs/js/iziToast.min.js';
+    iziToastScript.src = 'static/libs/js/iziToast.min.js';
     document.body.appendChild(iziToastScript);
 })
