0f0e96bb6ef7c38d7a7e118ee5455c2276f2ae75
NornsInteractive <NornsInteractive@ni.com>
2025-06-02 18:03:17 +0800

    资源修改

--- static/js/config.js
+++ static/js/config.js
@@ -115,47 +115,49 @@ const API_SITES = {
     //     name: '空内容测试源',
     //     adult: true
     // },
-    ckzy: {
-        api: 'https://ckzy.me/api.php/provide/vod',
-        name: 'CK资源',
-        adult: true
-    },
-    jkun: {
-        api: 'https://www.jkunzyapi.com/api.php/provide/vod',
-        name: 'jkun资源',
-        adult: true
-    },
-    bwzy: {
-        api: 'https://api.bwzym3u8.com/api.php/provide/vod/at/json',
-        name: '百万资源',
-        adult: true
-    },
-    souav: {
-        api: 'https://api.souavzy.vip/api.php/provide/vod',
-        name: 'souav资源',
-        adult: true
-    },
-    r155: {
-        api: 'https://155api.com/api.php/provide/vod',
-        name: '155资源',
-        adult: true
-    },
-    lsb: {
-        api: 'https://apilsbzy1.com/api.php/provide/vod',
-        name: 'lsb资源',
-        adult: true
-    },
-    huangcang: {
-        api: 'https://hsckzy.vip/api.php/provide/vod',
-        name: '黄色仓库',
-        adult: true,
-        detail: 'https://hsckzy.vip/api.php/provide/vod'
-    },
-    yutu: {
-        api: 'https://yutuzy10.com/api.php/provide/vod',
-        name: '玉兔资源',
-        adult: true
-    }
+
+    // 18+资源在转移后启用
+    // ckzy: {
+    //     api: 'https://ckzy.me/api.php/provide/vod',
+    //     name: 'CK资源',
+    //     adult: true
+    // },
+    // jkun: {
+    //     api: 'https://www.jkunzyapi.com/api.php/provide/vod',
+    //     name: 'jkun资源',
+    //     adult: true
+    // },
+    // bwzy: {
+    //     api: 'https://api.bwzym3u8.com/api.php/provide/vod/at/json',
+    //     name: '百万资源',
+    //     adult: true
+    // },
+    // souav: {
+    //     api: 'https://api.souavzy.vip/api.php/provide/vod',
+    //     name: 'souav资源',
+    //     adult: true
+    // },
+    // r155: {
+    //     api: 'https://155api.com/api.php/provide/vod',
+    //     name: '155资源',
+    //     adult: true
+    // },
+    // lsb: {
+    //     api: 'https://apilsbzy1.com/api.php/provide/vod',
+    //     name: 'lsb资源',
+    //     adult: true
+    // },
+    // huangcang: {
+    //     api: 'https://hsckzy.vip/api.php/provide/vod',
+    //     name: '黄色仓库',
+    //     adult: true,
+    //     detail: 'https://hsckzy.vip/api.php/provide/vod'
+    // },
+    // yutu: {
+    //     api: 'https://yutuzy10.com/api.php/provide/vod',
+    //     name: '玉兔资源',
+    //     adult: true
+    // },
 
     // 下面是资源失效率高的API源，不建议使用
     // subo: {
