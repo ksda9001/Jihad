1b10fb9c5e3c419d68eff32a8108a25a1b6c0eac
NornsInteractive <NornsInteractive@ni.com>
2025-06-02 18:01:02 +0800

    资源api修复

--- static/js/config.js
+++ static/js/config.js
@@ -155,7 +155,7 @@ const API_SITES = {
         api: 'https://yutuzy10.com/api.php/provide/vod',
         name: '玉兔资源',
         adult: true
-    },
+    }
 
     // 下面是资源失效率高的API源，不建议使用
     // subo: {
